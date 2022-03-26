221c778d4099112cc3d840a6ef8ec23d64240224
OatmealDome <julian@oatmealdome.me>
2022-03-26 02:33:40 -0400

    Host: Add basic Host function implementations for iOS

---
+++ Source/iOS/Library/Host.cpp
@@ -0,0 +1,73 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <string>
+#include <vector>
+
+#include "Core/Host.h"
+
+std::vector<std::string> Host_GetPreferredLocales()
+{
+  return {};
+}
+
+void Host_NotifyMapLoaded()
+{
+}
+
+void Host_RefreshDSPDebuggerWindow()
+{
+}
+
+void Host_Message(HostMessageID)
+{
+}
+
+void Host_UpdateTitle(const std::string&)
+{
+}
+
+void Host_UpdateDisasmDialog()
+{
+}
+
+void Host_UpdateMainFrame()
+{
+}
+
+void Host_RequestRenderWindowSize(int, int)
+{
+}
+
+bool Host_UIBlocksControllerState()
+{
+  return false;
+}
+
+bool Host_RendererHasFocus()
+{
+  return false;
+}
+
+bool Host_RendererHasFullFocus()
+{
+  return false;
+}
+
+bool Host_RendererIsFullscreen()
+{
+  return false;
+}
+
+void Host_YieldToUI()
+{
+}
+
+void Host_TitleChanged()
+{
+}
+
+std::unique_ptr<GBAHostInterface> Host_CreateGBAHost(std::weak_ptr<HW::GBA::Core> core)
+{
+  return nullptr;
+}
