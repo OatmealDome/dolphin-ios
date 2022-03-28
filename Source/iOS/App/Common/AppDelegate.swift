d7ce9768b0d6b80865615e03a60a9b0b139fbdf2
OatmealDome <julian@oatmealdome.me>
2022-03-28 18:58:41 -0400

    AppDelegate: Move to Swift

---
+++ Source/iOS/App/Common/AppDelegate.swift
@@ -0,0 +1,15 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate : UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    return true
+  }
+  
+}
