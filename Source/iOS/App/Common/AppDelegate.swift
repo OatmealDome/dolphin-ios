117c1de25906a19ce88d5aa332a25d5c791ab9e0
OatmealDome <julian@oatmealdome.me>
2022-03-28 20:07:15 -0400

    AppDelegate: Move to services model

--- Source/iOS/App/Common/AppDelegate.swift
+++ Source/iOS/App/Common/AppDelegate.swift
@@ -5,11 +5,67 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate : UIResponder, UIApplicationDelegate {
-  
   var window: UIWindow?
   
+  let services: [UIApplicationDelegate] = [
+    
+  ]
+  
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    return true
+    var returnedResult: Bool = true
+        
+    for service in services {
+      let result = service.application?(application, didFinishLaunchingWithOptions: launchOptions) ?? true
+      
+      if (!result) {
+        returnedResult = false
+      }
+    }
+    
+    return returnedResult;
   }
   
+  func applicationWillTerminate(_ application: UIApplication) {
+    for service in services {
+      service.applicationWillTerminate?(application)
+    }
+  }
+  
+  func applicationDidBecomeActive(_ application: UIApplication) {
+    for service in services {
+      service.applicationDidBecomeActive?(application)
+    }
+  }
+  
+  func applicationWillResignActive(_ application: UIApplication) {
+    for service in services {
+      service.applicationWillResignActive?(application)
+    }
+  }
+  
+  func applicationDidEnterBackground(_ application: UIApplication) {
+    for service in services {
+      service.applicationDidEnterBackground?(application)
+    }
+  }
+  
+  func applicationDidReceiveMemoryWarning(_ application: UIApplication) {
+    for service in services {
+      service.applicationDidReceiveMemoryWarning?(application)
+    }
+  }
+  
+  func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    var returnedResult: Bool = true
+    
+    for service in services {
+      let result = service.application?(app, open: url, options: options) ?? true
+      
+      if (!result) {
+        returnedResult = false
+      }
+    }
+    
+    return returnedResult;
+  }
 }
