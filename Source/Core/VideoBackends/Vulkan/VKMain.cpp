5fa4037a3ca738d9621f80b98aca6d650c36ef1b
OatmealDome <julian@oatmealdome.me>
2022-03-30 04:09:31 -0400

    VKMain: Don't create a CAMetalLayer on iOS

--- Source/Core/VideoBackends/Vulkan/VKMain.cpp
+++ Source/Core/VideoBackends/Vulkan/VKMain.cpp
@@ -306,7 +306,7 @@ static bool IsRunningOnMojaveOrHigher()
 
 void VideoBackend::PrepareWindow(WindowSystemInfo& wsi)
 {
-#if defined(VK_USE_PLATFORM_METAL_EXT)
+#if defined(VK_USE_PLATFORM_METAL_EXT) && !defined(IPHONEOS)
   // This is kinda messy, but it avoids having to write Objective C++ just to create a metal layer.
   id view = reinterpret_cast<id>(wsi.render_surface);
   Class clsCAMetalLayer = objc_getClass("CAMetalLayer");
