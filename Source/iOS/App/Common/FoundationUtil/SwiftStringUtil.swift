271bcebc3700b5b925342ed5fae356c3cc42e48c
OatmealDome <julian@oatmealdome.me>
2022-03-30 13:42:47 -0400

    SwiftStringUtil: Add utility method to access stringByAppendingPathComponent in Swift

---
+++ Source/iOS/App/Common/FoundationUtil/SwiftStringUtil.swift
@@ -0,0 +1,10 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+import Foundation
+
+extension String {
+  func stringByAppendingPathComponent(path: String) -> String {
+    return (self as NSString).appendingPathComponent(path)
+  }
+}
