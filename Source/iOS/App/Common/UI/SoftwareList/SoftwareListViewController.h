2bf412a2bb66dc7f2d9c66074c4a2ca34acd711f
OatmealDome <julian@oatmealdome.me>
2022-04-02 16:12:13 -0400

    SoftwareListViewController: Add initial implementation with game list

---
+++ Source/iOS/App/Common/UI/SoftwareList/SoftwareListViewController.h
@@ -0,0 +1,16 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#import <UIKit/UIKit.h>
+
+@class GameFilePtrWrapper;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SoftwareListViewController : UICollectionViewController<UICollectionViewDelegateFlowLayout> {
+  NSArray<GameFilePtrWrapper*>* _gameFiles;
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Source/iOS/App/Common/UI/SoftwareList/SoftwareListViewController.mm
@@ -0,0 +1,79 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#import "SoftwareListViewController.h"
+
+#import "FoundationStringUtil.h"
+#import "GameFileCacheManager.h"
+#import "GameFilePtrWrapper.h"
+#import "Swift.h"
+
+#import "UICommon/GameFile.h"
+
+@interface SoftwareListViewController ()
+
+@end
+
+@implementation SoftwareListViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  
+  self->_gameFiles = [[GameFileCacheManager sharedManager] getGames];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+  [super viewDidAppear:animated];
+  
+  [[GameFileCacheManager sharedManager] rescanAndFetchMetadataWithCompletionHandler:^{
+    dispatch_async(dispatch_get_main_queue(), ^{
+      self->_gameFiles = [[GameFileCacheManager sharedManager] getGames];
+      [self.collectionView reloadData];
+    });
+  }];
+}
+
+#pragma mark <UICollectionViewDataSource>
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)collectionView {
+  return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView*)collectionView numberOfItemsInSection:(NSInteger)section {
+  return self->_gameFiles.count;
+}
+
+- (UICollectionViewCell*)collectionView:(UICollectionView*)collectionView cellForItemAtIndexPath:(NSIndexPath*)indexPath {
+  SoftwareListCell* cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"softwareCell" forIndexPath:indexPath];
+  GameFilePtrWrapper* gameFileWrapper = [self->_gameFiles objectAtIndex:indexPath.item];
+  
+  NSString* gameName = CppToFoundationString(gameFileWrapper.gameFile->GetName(UICommon::GameFile::Variant::LongAndPossiblyCustom));
+  
+  const UICommon::GameCover& cover = gameFileWrapper.gameFile->GetCoverImage();
+  
+  UIImage* image;
+  if (cover.buffer.empty()) {
+    image = [UIImage imageNamed:@"NoCover"];
+  } else {
+    NSData* data = [NSData dataWithBytes:cover.buffer.data() length:cover.buffer.size()];
+    image = [UIImage imageWithData:data];
+  }
+  
+  cell.imageView.image = image;
+  cell.nameLabel.text = gameName;
+  
+  return cell;
+}
+
+#pragma mark <UICollectionViewDelegateFlowLayout>
+
+- (CGSize)collectionView:(UICollectionView*)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath*)indexPath {
+  const CGFloat cellsPerRow = 3.f;
+  const CGFloat cellSpacing = 4.f;
+  const CGFloat cellHeight = 218.f;
+  
+  CGFloat totalSpacing = ((cellsPerRow - 1) * cellSpacing);
+  return CGSizeMake((collectionView.bounds.size.width - totalSpacing) / cellsPerRow, cellHeight);
+}
+
+@end
