fede4423636eba367f3d9b194838aa9dff161498
OatmealDome <julian@oatmealdome.me>
2022-05-25 19:24:22 -0400

    DolphinCoreService: Add MsgAlert handler

--- Source/iOS/App/Common/Services/DolphinCoreService.mm
+++ Source/iOS/App/Common/Services/DolphinCoreService.mm
@@ -3,15 +3,78 @@
 
 #import "DolphinCoreService.h"
 
-#import "DolphiniOS-Swift.h"
-
-#import "FoundationStringUtil.h"
-
-#import "UICommon/UICommon.h"
 #import "Core/Config/UISettings.h"
 
+#import "Common/MsgHandler.h"
+
+#import "UICommon/UICommon.h"
+
+#import "DolphiniOS-Swift.h"
+#import "FoundationStringUtil.h"
 
 @implementation DolphinCoreService
+
+static bool MsgAlert(const char* caption, const char* text, bool question, Common::MsgType style)
+{
+  NSString* foundationCaption = CToFoundationString(caption);
+  NSString* foundationText = CToFoundationString(text);
+  
+  // Log to console as a backup
+  NSLog(@"MsgAlert - %@: %@ (question: %d)", foundationCaption, foundationText, question ? 1 : 0);
+
+  NSCondition* condition = [[NSCondition alloc] init];
+  
+  __block bool confirmed = false;
+  
+  dispatch_async(dispatch_get_main_queue(), ^{
+    UIWindow* window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    window.rootViewController = [[UIViewController alloc] init];
+    window.windowLevel = UIWindowLevelAlert;
+    
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:foundationCaption message:foundationText preferredStyle:UIAlertControllerStyleAlert];
+    
+    void (^finish)() = ^void() {
+      [condition signal];
+      [window setHidden:true];
+    };
+
+    if (question) {
+      [alert addAction:[UIAlertAction actionWithTitle:@"No" style:UIAlertActionStyleDefault
+        handler:^(UIAlertAction* action) {
+        confirmed = false;
+
+        finish();
+      }]];
+
+      [alert addAction:[UIAlertAction actionWithTitle:@"Yes" style:UIAlertActionStyleDefault
+        handler:^(UIAlertAction * action) {
+        confirmed = true;
+
+        finish();
+      }]];
+    }
+    else
+    {
+      [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+        handler:^(UIAlertAction* action) {
+        confirmed = true;
+        
+        finish();
+      }]];
+    }
+
+    [window makeKeyAndVisible];
+
+    [window.rootViewController presentViewController:alert animated:true completion:nil];
+  });
+
+  // Wait for a button press
+  [condition lock];
+  [condition wait];
+  [condition unlock];
+
+  return confirmed;
+}
 
 - (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary<UIApplicationLaunchOptionsKey,id>*)launchOptions {
   UICommon::SetUserDirectory(FoundationToCppString([UserFolderUtil getUserFolder]));
