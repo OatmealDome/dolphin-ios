036885b0b1241e89fd34659c23a2775a2a50d9b2
OatmealDome <julian@oatmealdome.me>
2022-05-25 22:16:48 -0400

    EmulationViewController: Move emulation init into a function

--- Source/iOS/App/Common/UI/Emulation/EmulationViewController.mm
+++ Source/iOS/App/Common/UI/Emulation/EmulationViewController.mm
@@ -13,32 +13,45 @@
 
 @interface EmulationViewController ()
 
+- (void)runEmulation;
+
 @end
 
 @implementation EmulationViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
+  [super viewDidLoad];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
-  WindowSystemInfo wsi;
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+    [self runEmulation];
+  });
+}
+
+- (void)runEmulation {
+  __block WindowSystemInfo wsi;
   wsi.type = WindowSystemType::iOS;
-  wsi.render_surface = (__bridge void*)self.metalView.layer;
+  
+  dispatch_sync(dispatch_get_main_queue(), ^{
+    wsi.render_surface = (__bridge void*)self.metalView.layer;
+  });
+  
   wsi.render_surface_scale = UIScreen.mainScreen.scale;
   
   std::unique_ptr<BootParameters> boot = [self.bootParameter generateDolphinBootParameter];
   
-  if (BootManager::BootCore(std::move(boot), wsi)) {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-      while (Core::GetState() == Core::State::Starting) {
-        [NSThread sleepForTimeInterval:0.025];
-      }
-      
-      while (Core::IsRunning()) {
-        Core::HostDispatchJobs();
-      }
-    });
+  if (!BootManager::BootCore(std::move(boot), wsi)) {
+    PanicAlertFmt("Failed to init core!");
+    return;
+  }
+  
+  while (Core::GetState() == Core::State::Starting) {
+    [NSThread sleepForTimeInterval:0.025];
+  }
+  
+  while (Core::IsRunning()) {
+    Core::HostDispatchJobs();
   }
 }
 
