0005ed309038cdbabd10ec84539ca52edc93d523
OatmealDome <julian@oatmealdome.me>
2022-05-30 01:30:39 -0400

    MainSettings: Add CoreAudio backend define

--- Source/Core/Core/Config/MainSettings.h
+++ Source/Core/Core/Config/MainSettings.h
@@ -20,6 +20,7 @@
 #define BACKEND_PULSEAUDIO "Pulse"
 #define BACKEND_OPENSLES "OpenSLES"
 #define BACKEND_WASAPI _trans("WASAPI (Exclusive Mode)")
+#define BACKEND_COREAUDIO "CoreAudio"
 
 namespace PowerPC
 {
