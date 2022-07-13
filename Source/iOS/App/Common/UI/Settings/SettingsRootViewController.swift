79af30a11419125bda37d5fb58e45046688ae948
OatmealDome <julian@oatmealdome.me>
2022-07-13 19:32:09 -0400

    SettingsRoot: Display Dolphin core version

--- Source/iOS/App/Common/UI/Settings/SettingsRootViewController.swift
+++ Source/iOS/App/Common/UI/Settings/SettingsRootViewController.swift
@@ -6,13 +6,19 @@ import UIKit
 
 class SettingsRootViewController : UITableViewController {
   @IBOutlet weak var versionLabel: UILabel!
+  @IBOutlet weak var coreVersionLabel: UILabel!
   
   override func viewDidLoad() {
     let infoDict = Bundle.main.infoDictionary!
+    
     let version: String = infoDict["CFBundleShortVersionString"] as! String
     let revision: String = infoDict["CFBundleVersion"] as! String
     
     versionLabel.text = String(format: "%@ (%@)", version, revision)
+    
+    let coreVersion: String = infoDict["DOLCoreVersion"] as! String
+    
+    coreVersionLabel.text = coreVersion
   }
   
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
