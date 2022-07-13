75d11b6dbca4321fcec5deb555c86c24280a47e1
OatmealDome <julian@oatmealdome.me>
2022-07-13 18:14:33 -0400

    SettingsRoot: Implement Settings root menu

---
+++ Source/iOS/App/Common/UI/Settings/SettingsRootViewController.swift
@@ -0,0 +1,25 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+import Foundation
+import UIKit
+
+class SettingsRootViewController : UITableViewController {
+  @IBOutlet weak var versionLabel: UILabel!
+  
+  override func viewDidLoad() {
+    let infoDict = Bundle.main.infoDictionary!
+    let version: String = infoDict["CFBundleShortVersionString"] as! String
+    let revision: String = infoDict["CFBundleVersion"] as! String
+    
+    versionLabel.text = String(format: "%@ (%@)", version, revision)
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    
+    if (indexPath.section == 0 && indexPath.row == 2) {
+      UIApplication.shared.open(URL(string: "https://oatmealdome.me/dolphinios/")!)
+    }
+  }
+}
