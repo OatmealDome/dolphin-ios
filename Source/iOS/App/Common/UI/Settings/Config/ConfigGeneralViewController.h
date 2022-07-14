be2c0e3a0c406d30775d102f15948666d733ebf5
OatmealDome <julian@oatmealdome.me>
2022-07-13 22:44:30 -0400

    ConfigSettings: Implement switches on General tab

---
+++ Source/iOS/App/Common/UI/Settings/Config/ConfigGeneralViewController.h
@@ -0,0 +1,19 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#import <UIKit/UIKit.h>
+
+#import "DOLSwitch.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ConfigGeneralViewController : UITableViewController
+
+@property (weak, nonatomic) IBOutlet DOLSwitch* dualCoreSwitch;
+@property (weak, nonatomic) IBOutlet DOLSwitch* cheatsSwitch;
+@property (weak, nonatomic) IBOutlet DOLSwitch* mismatchedRegionSwitch;
+@property (weak, nonatomic) IBOutlet DOLSwitch* changeDiscsSwitch;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Source/iOS/App/Common/UI/Settings/Config/ConfigGeneralViewController.mm
@@ -0,0 +1,46 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#import "ConfigGeneralViewController.h"
+
+#import "Core/Config/MainSettings.h"
+
+@interface ConfigGeneralViewController ()
+
+@end
+
+@implementation ConfigGeneralViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  
+  [self.dualCoreSwitch setOn:Config::Get(Config::MAIN_CPU_THREAD)];
+  [self.dualCoreSwitch addValueChangedTarget:self action:@selector(dualCoreChanged)];
+  
+  [self.cheatsSwitch setOn:Config::Get(Config::MAIN_ENABLE_CHEATS)];
+  [self.cheatsSwitch addValueChangedTarget:self action:@selector(cheatsChanged)];
+  
+  [self.mismatchedRegionSwitch setOn:Config::Get(Config::MAIN_OVERRIDE_REGION_SETTINGS)];
+  [self.mismatchedRegionSwitch addValueChangedTarget:self action:@selector(mismatchedRegionChanged)];
+  
+  [self.changeDiscsSwitch setOn:Config::Get(Config::MAIN_AUTO_DISC_CHANGE)];
+  [self.changeDiscsSwitch addValueChangedTarget:self action:@selector(changeDiscsChanged)];
+}
+
+- (void)dualCoreChanged {
+  Config::SetCurrent(Config::MAIN_CPU_THREAD, [self.dualCoreSwitch isOn]);
+}
+
+- (void)cheatsChanged {
+  Config::SetCurrent(Config::MAIN_ENABLE_CHEATS, [self.cheatsSwitch isOn]);
+}
+
+- (void)mismatchedRegionChanged {
+  Config::SetCurrent(Config::MAIN_OVERRIDE_REGION_SETTINGS, [self.mismatchedRegionSwitch isOn]);
+}
+
+- (void)changeDiscsChanged {
+  Config::SetCurrent(Config::MAIN_AUTO_DISC_CHANGE, [self.changeDiscsSwitch isOn]);
+}
+
+@end
