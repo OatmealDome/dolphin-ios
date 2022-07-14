85b4f0e83fd7526d375cd60afa878234c50f88fe
OatmealDome <julian@oatmealdome.me>
2022-07-14 13:44:19 -0400

    ConfigSettings: Implement GameCube tab

---
+++ Source/iOS/App/Common/UI/Settings/Config/GameCube/ConfigGameCubeViewController.h
@@ -0,0 +1,17 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#import <UIKit/UIKit.h>
+
+#import "DOLSwitch.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ConfigGameCubeViewController : UITableViewController
+
+@property (weak, nonatomic) IBOutlet DOLUIKitSwitch* mainMenuSwitch;
+@property (weak, nonatomic) IBOutlet UILabel* languageLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Source/iOS/App/Common/UI/Settings/Config/GameCube/ConfigGameCubeViewController.mm
@@ -0,0 +1,67 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#import "ConfigGameCubeViewController.h"
+
+#import "Common/CommonPaths.h"
+#import "Common/FileUtil.h"
+
+#import "Core/Config/MainSettings.h"
+
+@interface ConfigGameCubeViewController ()
+
+@end
+
+@implementation ConfigGameCubeViewController
+
+- (void)viewWillAppear:(BOOL)animated {
+  [super viewWillAppear:animated];
+  
+  self.mainMenuSwitch.on = Config::Get(Config::MAIN_SKIP_IPL);
+  [self.mainMenuSwitch addValueChangedTarget:self action:@selector(mainMenuChanged)];
+  
+  bool haveMenu = false;
+  for (const std::string dir : {USA_DIR, JAP_DIR, EUR_DIR}) {
+    const auto path = DIR_SEP + dir + DIR_SEP GC_IPL;
+    if (File::Exists(File::GetUserPath(D_GCUSER_IDX) + path) ||
+        File::Exists(File::GetSysDirectory() + GC_SYS_DIR + path)) {
+      haveMenu = true;
+      break;
+    }
+  }
+  
+  self.mainMenuSwitch.enabled = haveMenu;
+  
+  NSString* language;
+  switch (Config::Get(Config::MAIN_GC_LANGUAGE)) {
+    case 0:
+      language = @"English";
+      break;
+    case 1:
+      language = @"German";
+      break;
+    case 2:
+      language = @"French";
+      break;
+    case 3:
+      language = @"Spanish";
+      break;
+    case 4:
+      language = @"Italian";
+      break;
+    case 5:
+      language = @"Dutch";
+      break;
+    default:
+      language = @"Error";
+      break;
+  }
+  
+  self.languageLabel.text = language;
+}
+
+- (void)mainMenuChanged {
+  Config::SetBaseOrCurrent(Config::MAIN_SKIP_IPL, self.mainMenuSwitch.on);
+}
+
+@end
