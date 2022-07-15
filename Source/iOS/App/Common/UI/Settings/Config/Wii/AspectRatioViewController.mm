85e05532e8c2cca7c030f608d600bc7f1d4b752e
OatmealDome <julian@oatmealdome.me>
2022-07-15 02:59:10 -0400

    ConfigSettings: Implement Aspect Ratio dropdown in Wii tab

---
+++ Source/iOS/App/Common/UI/Settings/Config/Wii/AspectRatioViewController.mm
@@ -0,0 +1,42 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#import "AspectRatioViewController.h"
+
+#import "Core/Config/SYSCONFSettings.h"
+
+@interface AspectRatioViewController ()
+
+@end
+
+@implementation AspectRatioViewController {
+  NSInteger _lastSelected;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+  [super viewWillAppear:animated];
+  
+  _lastSelected = Config::Get(Config::SYSCONF_WIDESCREEN) ? 1 : 0;
+  
+  UITableViewCell* cell = [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:_lastSelected inSection:0]];
+  [cell setAccessoryType:UITableViewCellAccessoryCheckmark];
+
+}
+
+- (void)tableView:(UITableView*)tableView didSelectRowAtIndexPath:(NSIndexPath*)indexPath {
+  if (_lastSelected != indexPath.row) {
+    Config::SetBase(Config::SYSCONF_WIDESCREEN, indexPath.row == 1);
+    
+    UITableViewCell* cell = [tableView cellForRowAtIndexPath:indexPath];
+    cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    
+    UITableViewCell* oldCell = [tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:_lastSelected inSection:0]];
+    oldCell.accessoryType = UITableViewCellAccessoryNone;
+    
+    _lastSelected = indexPath.row;
+  }
+  
+  [tableView deselectRowAtIndexPath:indexPath animated:true];
+}
+
+@end
