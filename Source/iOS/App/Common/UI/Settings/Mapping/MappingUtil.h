5cf58e7b1a738f188501571ce1dd17d202671055
OatmealDome <julian@oatmealdome.me>
2022-07-21 20:16:10 -0400

    MappingUtil: Add method to detect expressions

--- Source/iOS/App/Common/UI/Settings/Mapping/MappingUtil.h
+++ Source/iOS/App/Common/UI/Settings/Mapping/MappingUtil.h
@@ -3,13 +3,35 @@
 
 #import <Foundation/Foundation.h>
 
+#import <string>
+#import <vector>
+
 #import "Core/HW/WiimoteEmu/WiimoteEmu.h"
+
+namespace ciface {
+namespace Core {
+class DeviceContainer;
+class DeviceQualifier;
+}
+
+namespace MappingCommon {
+enum class Quote;
+}
+}
+
+@class UIViewController;
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface MappingUtil : NSObject
 
 + (NSString*)getLocalizedStringForWiimoteExtension:(WiimoteEmu::ExtensionNumber)extension;
+
++ (void)detectExpressionWithDefaultDevice:(const ciface::Core::DeviceQualifier&)defaultDevice
+                               allDevices:(bool)allDevices
+                                    quote:(ciface::MappingCommon::Quote)quote
+                           viewController:(UIViewController*)viewController
+                                 callback:(void (^)(std::string))callback;
 
 @end
 
