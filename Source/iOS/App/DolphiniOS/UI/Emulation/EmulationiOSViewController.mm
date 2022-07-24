902f434eee61c9a639f6f0e3d4f9aadfd1539c11
OatmealDome <julian@oatmealdome.me>
2022-07-24 04:42:19 -0400

    EmulationiOSViewController: Add subclass of EmulationViewController for iOS

---
+++ Source/iOS/App/DolphiniOS/UI/Emulation/EmulationiOSViewController.mm
@@ -0,0 +1,12 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#import "EmulationiOSViewController.h"
+
+@interface EmulationiOSViewController ()
+
+@end
+
+@implementation EmulationiOSViewController
+
+@end
