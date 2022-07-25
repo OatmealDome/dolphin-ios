c8380529caf87f9a1fecc6cd32f455a57002b437
OatmealDome <julian@oatmealdome.me>
2022-07-25 19:15:55 -0400

    Config: Add empty iOSSettings files for future use

---
+++ Source/Core/Core/Config/iOSSettings.cpp
@@ -0,0 +1,8 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "Core/Config/iOSSettings.h"
+
+namespace Config
+{
+}  // namespace Config
---
+++ Source/Core/Core/Config/iOSSettings.h
@@ -0,0 +1,10 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "Common/Config/Config.h"
+
+namespace Config
+{
+}  // namespace Config
