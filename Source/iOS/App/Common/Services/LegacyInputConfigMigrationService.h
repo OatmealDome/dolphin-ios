b67d6c60b519653b9a209d7d08b8495d7a513599
OatmealDome <julian@oatmealdome.me>
2022-12-31 14:58:08 -0500

    LegacyInputConfigMigrationService: Add service to handle migration of old controller configs

---
+++ Source/iOS/App/Common/Services/LegacyInputConfigMigrationService.h
@@ -0,0 +1,14 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#import <Foundation/Foundation.h>
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LegacyInputConfigMigrationService : UIResponder <UIApplicationDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Source/iOS/App/Common/Services/LegacyInputConfigMigrationService.mm
@@ -0,0 +1,52 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#import "LegacyInputConfigMigrationService.h"
+
+#import "Common/FileUtil.h"
+#import "Common/IniFile.h"
+
+#import "Core/HW/GCPad.h"
+#import "Core/HW/Wiimote.h"
+
+#import "InputCommon/ControllerEmu/ControllerEmu.h"
+#import "InputCommon/InputConfig.h"
+
+@implementation LegacyInputConfigMigrationService
+
+- (void)convertInputConfig:(InputConfig*)config {
+  for (int i = 0; i < 4; i++) {
+    ControllerEmu::EmulatedController* controller = config->GetController(i);
+    
+    const ciface::Core::DeviceQualifier& qualifier = controller->GetDefaultDevice();
+    
+    if (qualifier.source != "Android" || qualifier.name != "Touchscreen") {
+      continue;
+    }
+    
+    IniFile iniFile;
+    
+    // We no longer support Touchscreen devices on ports other than the first one.
+    // If this is not the first device, we will skip loading the INI file, which will cause
+    // the code to import a blank profile.
+    if (i == 0) {
+      const std::string builtInPath = File::GetSysDirectory() + "Profiles/" + config->GetProfileName() + "/Touchscreen.ini";
+      
+      iniFile.Load(builtInPath);
+    }
+    
+    controller->LoadConfig(iniFile.GetOrCreateSection("Profile"));
+    controller->UpdateReferences(g_controller_interface);
+  }
+  
+  config->SaveConfig();
+}
+
+- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary<UIApplicationLaunchOptionsKey,id>*)launchOptions {
+  [self convertInputConfig:Pad::GetConfig()];
+  [self convertInputConfig:Wiimote::GetConfig()];
+  
+  return true;
+}
+
+@end
