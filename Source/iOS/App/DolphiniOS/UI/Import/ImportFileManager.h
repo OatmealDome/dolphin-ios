839cf513bb959a2583d1ce5b8e3b31616e749472
OatmealDome <julian@oatmealdome.me>
2022-12-31 01:52:49 -0500

    ImportFileManager: Add manager class to deal with importing software

---
+++ Source/iOS/App/DolphiniOS/UI/Import/ImportFileManager.h
@@ -0,0 +1,19 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#import <Foundation/Foundation.h>
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ImportFileManager : NSObject
+
++ (ImportFileManager*)shared;
+
+- (void)registerMainDisplayScene:(nullable UIWindowScene*)scene;
+- (void)importFileAtUrl:(NSURL*)url;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Source/iOS/App/DolphiniOS/UI/Import/ImportFileManager.m
@@ -0,0 +1,140 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#import "ImportFileManager.h"
+
+#import "Swift.h"
+
+#import "LocalizationUtil.h"
+
+@implementation ImportFileManager {
+  __weak UIWindowScene* _mainScene;
+  UIWindow* _window;
+}
+
++ (ImportFileManager*)shared {
+  static ImportFileManager* sharedInstance = nil;
+  static dispatch_once_t onceToken;
+
+  dispatch_once(&onceToken, ^{
+    sharedInstance = [[self alloc] init];
+  });
+
+  return sharedInstance;
+}
+
+- (void)registerMainDisplayScene:(nullable UIWindowScene*)scene {
+  _mainScene = scene;
+}
+
+- (void)showWindow {
+  self->_window = [[UIWindow alloc] initWithWindowScene:self->_mainScene];
+  self->_window.frame = [UIScreen mainScreen].bounds;
+  self->_window.rootViewController = [[UIViewController alloc] init];
+  self->_window.windowLevel = UIWindowLevelAlert;
+  
+  UIWindow* topWindow = self->_mainScene.windows.lastObject;
+  self->_window.windowLevel = topWindow.windowLevel + 1;
+  
+  [self->_window makeKeyAndVisible];
+}
+
+- (void)hideWindow {
+  [self->_window setHidden:true];
+  
+  self->_window = nil;
+}
+
+- (void)presentViewControllerOnWindow:(UIViewController*)controller {
+  [self->_window.rootViewController presentViewController:controller animated:true completion:nil];
+}
+
+- (void)importFileAtUrl:(NSURL*)url {
+  if (_mainScene == nil) {
+    return;
+  }
+  
+  [self showWindow];
+  
+  if (![url startAccessingSecurityScopedResource]) {
+    UIAlertController* errorAlert = [UIAlertController alertControllerWithTitle:DOLCoreLocalizedString(@"Error") message:@"Failed to start accessing security scoped resource." preferredStyle:UIAlertControllerStyleAlert];
+    
+    [errorAlert addAction:[UIAlertAction actionWithTitle:DOLCoreLocalizedString(@"OK") style:UIAlertActionStyleDefault
+      handler:^(UIAlertAction* action) {
+      [self hideWindow];
+    }]];
+    
+    [self presentViewControllerOnWindow:errorAlert];
+    
+    return;
+  }
+  
+  void (^finish)(void) = ^void() {
+    [url stopAccessingSecurityScopedResource];
+    
+    [self hideWindow];
+  };
+  
+  NSString* sourcePath = [url path];
+  NSString* destinationPath = [[UserFolderUtil getSoftwareFolder] stringByAppendingPathComponent:[sourcePath lastPathComponent]];
+  
+  NSFileManager* fileManager = [NSFileManager defaultManager];
+  
+  if ([fileManager fileExistsAtPath:destinationPath]) {
+    UIAlertController* errorAlert = [UIAlertController alertControllerWithTitle:DOLCoreLocalizedString(@"Error") message:@"This software has already been imported." preferredStyle:UIAlertControllerStyleAlert];
+    
+    [errorAlert addAction:[UIAlertAction actionWithTitle:DOLCoreLocalizedString(@"OK") style:UIAlertActionStyleDefault
+      handler:^(UIAlertAction* action) {
+      finish();
+    }]];
+    
+    [self presentViewControllerOnWindow:errorAlert];
+    
+    return;
+  }
+  
+  UIAlertController* alert = [UIAlertController alertControllerWithTitle:DOLCoreLocalizedString(@"Import") message:nil preferredStyle:UIAlertControllerStyleAlert];
+
+  [alert addAction:[UIAlertAction actionWithTitle:DOLCoreLocalizedString(@"Copy") style:UIAlertActionStyleDefault
+    handler:^(UIAlertAction* action) {
+    NSError* error = nil;
+    if (![fileManager copyItemAtPath:sourcePath toPath:destinationPath error:&error]) {
+      UIAlertController* errorAlert = [UIAlertController alertControllerWithTitle:DOLCoreLocalizedString(@"Error") message:[NSString stringWithFormat:@"The copy operation failed.\n\n%@", error.localizedDescription] preferredStyle:UIAlertControllerStyleAlert];
+      
+      [errorAlert addAction:[UIAlertAction actionWithTitle:DOLCoreLocalizedString(@"OK") style:UIAlertActionStyleDefault
+        handler:^(UIAlertAction* action) {
+        finish();
+      }]];
+      
+      [self presentViewControllerOnWindow:errorAlert];
+    } else {
+      finish();
+    }
+  }]];
+  
+  [alert addAction:[UIAlertAction actionWithTitle:DOLCoreLocalizedString(@"Move") style:UIAlertActionStyleDefault
+    handler:^(UIAlertAction* action) {
+    NSError* error = nil;
+    if (![fileManager moveItemAtPath:sourcePath toPath:destinationPath error:&error]) {
+      UIAlertController* errorAlert = [UIAlertController alertControllerWithTitle:DOLCoreLocalizedString(@"Error") message:[NSString stringWithFormat:@"The move operation failed.\n\n%@", error.localizedDescription] preferredStyle:UIAlertControllerStyleAlert];
+      
+      [errorAlert addAction:[UIAlertAction actionWithTitle:DOLCoreLocalizedString(@"OK") style:UIAlertActionStyleDefault
+        handler:^(UIAlertAction* action) {
+        finish();
+      }]];
+      
+      [self presentViewControllerOnWindow:errorAlert];
+    } else {
+      finish();
+    }
+  }]];
+  
+  [alert addAction:[UIAlertAction actionWithTitle:DOLCoreLocalizedString(@"Cancel") style:UIAlertActionStyleCancel
+    handler:^(UIAlertAction* action) {
+    finish();
+  }]];
+  
+  [self presentViewControllerOnWindow:alert];
+}
+
+@end
