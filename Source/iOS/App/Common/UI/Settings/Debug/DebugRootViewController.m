284e91e185ffa52404a07f6b966fc76ad23e9bf6
OatmealDome <julian@oatmealdome.me>
2022-12-31 18:10:57 -0500

    DebugRootViewController: Add utility option to reset launch_times

--- Source/iOS/App/Common/UI/Settings/Debug/DebugRootViewController.m
+++ Source/iOS/App/Common/UI/Settings/Debug/DebugRootViewController.m
@@ -22,4 +22,20 @@
   [VirtualMFiControllerManager shared].shouldConnectController = self.mfiSwitch.on;
 }
 
+- (void)tableView:(UITableView*)tableView didSelectRowAtIndexPath:(NSIndexPath*)indexPath {
+  if (indexPath.section == 1 && indexPath.row == 0) { // Reset Launch Times
+    [[NSUserDefaults standardUserDefaults] setInteger:0 forKey:@"launch_times"];
+    
+    UIAlertController* launchAlert = [UIAlertController alertControllerWithTitle:@"Reset" message:@"launch_times was reset to 0." preferredStyle:UIAlertControllerStyleAlert];
+    
+    [launchAlert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction* action) {
+      //
+    }]];
+    
+    [self presentViewController:launchAlert animated:true completion:nil];
+  }
+  
+  [self.tableView deselectRowAtIndexPath:indexPath animated:true];
+}
+
 @end
