a0d1e96d246cbc4595ec2b4fc84a6d5b3e5aa83a
OatmealDome <julian@oatmealdome.me>
2022-12-31 00:33:14 -0500

    SoftwareListiOSViewController: Add iOS-specific software list view controller

---
+++ Source/iOS/App/DolphiniOS/UI/SoftwareList/SoftwareListiOSViewController.h
@@ -0,0 +1,12 @@
+// Copyright 2022 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#import "SoftwareListViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SoftwareListiOSViewController : SoftwareListViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
