90e3962bc184d2817cf5d0144ccfb7a452859540
OatmealDome <julian@oatmealdome.me>
2023-01-01 18:23:01 -0500

    SoftwareListiOSViewController: Add More menu containing Wii update functions

--- Source/iOS/App/DolphiniOS/UI/SoftwareList/SoftwareListiOSViewController.mm
+++ Source/iOS/App/DolphiniOS/UI/SoftwareList/SoftwareListiOSViewController.mm
@@ -3,9 +3,23 @@
 
 #import "SoftwareListiOSViewController.h"
 
+#import "Core/CommonTitles.h"
+#import "Core/IOS/ES/ES.h"
+#import "Core/IOS/IOS.h"
+
+#import "FoundationStringUtil.h"
 #import "ImportFileManager.h"
+#import "LocalizationUtil.h"
 
 @implementation SoftwareListiOSViewController
+
+- (void)viewDidAppear:(BOOL)animated {
+  [super viewDidAppear:animated];
+  
+  if (@available(iOS 14.0, *)) {
+    [self recreateMoreMenu];
+  }
+}
 
 - (IBAction)addButtonPressed:(id)sender {
   NSArray* types = @[
@@ -24,6 +38,57 @@
 
 - (void)documentPicker:(UIDocumentPickerViewController*)controller didPickDocumentsAtURLs:(NSArray<NSURL*>*)urls {
   [[ImportFileManager shared] importFileAtUrl:urls[0]];
+}
+
+- (void)recreateMoreMenu API_AVAILABLE(ios(14.0)) {
+  NSArray<UIMenuElement*>* wiiActions;
+  
+  // Get the system menu TMD
+  IOS::HLE::Kernel ios;
+  const auto tmd = ios.GetES()->FindInstalledTMD(Titles::SYSTEM_MENU);
+  
+  if (tmd.IsValid()) {
+    NSString* loadFormat;
+    
+    if (tmd.IsvWii()) {
+      loadFormat = DOLCoreLocalizedStringWithArgs(@"Load vWii System Menu %1", @"@");
+    } else {
+      loadFormat = DOLCoreLocalizedStringWithArgs(@"Load Wii System Menu %1", @"@");
+    }
+    
+    std::string version = DiscIO::GetSysMenuVersionString(tmd.GetTitleVersion(), tmd.IsvWii());
+    
+    wiiActions = @[
+      [UIAction actionWithTitle:[NSString stringWithFormat:loadFormat, CppToFoundationString(version)] image:[UIImage systemImageNamed:@"tray.and.arrow.down"] identifier:nil handler:^(UIAction*) {
+        // TODO
+      }],
+      [UIAction actionWithTitle:DOLCoreLocalizedString(@"Perform Online System Update") image:[UIImage systemImageNamed:@"icloud.and.arrow.down"] identifier:nil handler:^(UIAction*) {
+        [self performSegueForWiiUpdateWithSource:@"" isOnline:true];
+      }]
+    ];
+  } else {
+    wiiActions = @[
+      [UIMenu menuWithTitle:DOLCoreLocalizedString(@"Perform Online System Update") image:[UIImage systemImageNamed:@"icloud.and.arrow.down"] identifier:nil options:0 children:@[
+        [UIAction actionWithTitle:DOLCoreLocalizedString(@"Europe") image:nil identifier:nil handler:^(UIAction*) {
+          [self performSegueForWiiUpdateWithSource:@"EUR" isOnline:true];
+        }],
+        [UIAction actionWithTitle:DOLCoreLocalizedString(@"Japan") image:nil identifier:nil handler:^(UIAction*) {
+          [self performSegueForWiiUpdateWithSource:@"JPN" isOnline:true];
+        }],
+        [UIAction actionWithTitle:DOLCoreLocalizedString(@"Korea") image:nil identifier:nil handler:^(UIAction*) {
+          [self performSegueForWiiUpdateWithSource:@"KOR" isOnline:true];
+        }],
+        [UIAction actionWithTitle:DOLCoreLocalizedString(@"United States") image:nil identifier:nil handler:^(UIAction*) {
+          [self performSegueForWiiUpdateWithSource:@"USA" isOnline:true];
+        }]
+      ]
+    ]];
+  }
+  
+  
+  self.navigationItem.leftBarButtonItem.menu = [UIMenu menuWithChildren:@[
+    [UIMenu menuWithTitle:DOLCoreLocalizedString(@"Wii") image:nil identifier:nil options:UIMenuOptionsDisplayInline children:wiiActions]
+  ]];
 }
 
 @end
