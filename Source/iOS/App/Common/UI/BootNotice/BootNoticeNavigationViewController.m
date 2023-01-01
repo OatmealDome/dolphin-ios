3d72307cc4452ba48b71921f94adfb82cda257b8
OatmealDome <julian@oatmealdome.me>
2023-01-01 02:12:22 -0500

    BootNoticeNavigationViewController: Port from legacy codebase and clean up

---
+++ Source/iOS/App/Common/UI/BootNotice/BootNoticeNavigationViewController.m
@@ -0,0 +1,26 @@
+// Copyright 2023 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#import "BootNoticeNavigationViewController.h"
+
+@interface BootNoticeNavigationViewController ()
+
+@end
+
+@implementation BootNoticeNavigationViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view.
+}
+
+- (UIViewController*)popViewControllerAnimated:(BOOL)animated {
+  // Dismiss ourselves if we are popping the last view controller.
+  if (self.viewControllers.count == 1) {
+    [self dismissViewControllerAnimated:true completion:nil];
+  }
+  
+  return [self popViewControllerAnimated:animated];
+}
+
+@end
