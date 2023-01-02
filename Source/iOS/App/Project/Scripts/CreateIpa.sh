03e77fbd072427b69514f878a99fb38b8f29dab1
OatmealDome <julian@oatmealdome.me>
2023-01-02 17:59:38 -0500

    Project: Add script to create an IPA file

---
+++ Source/iOS/App/Project/Scripts/CreateIpa.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+BASE_DIR=$(mktemp -d)
+CURRENT_DIR=$(pwd)
+
+echo "Temporary directory at $BASE_DIR"
+
+APP_BUNDLE_PATH=$1
+SIGNING_CERTIFICATE=$2
+ENTITLEMENTS_PATH=$3
+OUTPUT_FILE=$4
+
+mkdir "$BASE_DIR/Payload"
+
+cp -R "$APP_BUNDLE_PATH" "$BASE_DIR/Payload/"
+
+codesign -f -s "$SIGNING_CERTIFICATE" --entitlements "$ENTITLEMENTS_PATH" "$BASE_DIR/Payload/DolphiniOS.app"
+
+cd "$BASE_DIR"
+
+zip -r "$OUTPUT_FILE" .
+
+cd "$CURRENT_DIR"
+
+echo "Cleaning up"
+
+rm -rf "$BASE_DIR"
+
+echo "Done"
