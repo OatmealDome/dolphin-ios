85cd13496e401f7ee1a1e36c1c71eb5000563e94
OatmealDome <julian@oatmealdome.me>
2023-01-04 15:15:29 -0500

    FirebaseService: Initialize Firebase analytics

--- Source/iOS/App/Common/Services/FirebaseService.mm
+++ Source/iOS/App/Common/Services/FirebaseService.mm
@@ -3,9 +3,14 @@
 
 #import "FirebaseService.h"
 
+#import <FirebaseAnalytics/FirebaseAnalytics.h>
 #import <FirebaseCore/FirebaseCore.h>
 
+#import "Core/Config/MainSettings.h"
+
+#import "BootNoticeManager.h"
 #import "Swift.h"
+#import "AnalyticsNoticeViewController.h"
 
 @implementation FirebaseService
 
@@ -15,6 +20,20 @@
   }
   
   [FIRApp configure];
+  
+  bool analyticsEnabled;
+  
+  if (!Config::GetBase(Config::MAIN_ANALYTICS_PERMISSION_ASKED)) {
+    // Default to no analytics temporarily
+    analyticsEnabled = false;
+    
+    AnalyticsNoticeViewController* controller = [[AnalyticsNoticeViewController alloc] initWithNibName:@"AnalyticsNotice" bundle:nil];
+    [[BootNoticeManager shared] enqueueViewController:controller];
+  } else {
+    analyticsEnabled = Config::GetBase(Config::MAIN_ANALYTICS_ENABLED);
+  }
+  
+  [FIRAnalytics setAnalyticsCollectionEnabled:analyticsEnabled];
   
   return true;
 }
