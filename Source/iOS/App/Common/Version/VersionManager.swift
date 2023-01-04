54130d7b6b9c7cd3f4735ae740123099862e2094
OatmealDome <julian@oatmealdome.me>
2023-01-04 03:40:58 -0500

    VersionManager: Add manager class to hold version information

---
+++ Source/iOS/App/Common/Version/VersionManager.swift
@@ -0,0 +1,31 @@
+// Copyright 2023 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+import Foundation
+
+@objc class VersionManager: NSObject {
+  
+  private static let sharedInstance = VersionManager()
+  
+  class func shared() -> VersionManager {
+    return sharedInstance
+  }
+  
+  private(set) var version: String // "4.0.0"
+  private(set) var build: Int // 1
+  
+  private(set) var userFacingVersion: String // "4.0.0 (1)"
+  
+  private(set) var coreVersion: String // "5.0-12345"
+  
+  private override init() {
+    let info = Bundle.main.infoDictionary!
+    
+    version = info["CFBundleShortVersionString"] as! String
+    build = Int(info["CFBundleVersion"] as! String)!
+    
+    userFacingVersion = String(format: "%@ (%d)", version, build)
+    
+    coreVersion = info["DOLCoreVersion"] as! String
+  }
+}
