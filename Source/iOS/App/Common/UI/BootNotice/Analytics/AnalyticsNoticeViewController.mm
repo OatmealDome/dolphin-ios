09af364e34f08482333a66bd1a02b90ab9514353
OatmealDome <julian@oatmealdome.me>
2023-01-04 15:08:38 -0500

    AnalyticsNotice: Add notice for analytics opt-in

---
+++ Source/iOS/App/Common/UI/BootNotice/Analytics/AnalyticsNoticeViewController.mm
@@ -0,0 +1,37 @@
+// Copyright 2023 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#import "AnalyticsNoticeViewController.h"
+
+#import <FirebaseAnalytics/FirebaseAnalytics.h>
+
+#import "Core/Config/MainSettings.h"
+
+@interface AnalyticsNoticeViewController ()
+
+@end
+
+@implementation AnalyticsNoticeViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+}
+
+- (void)HandleResponse:(bool)response {
+  Config::SetBaseOrCurrent(Config::MAIN_ANALYTICS_PERMISSION_ASKED, true);
+  Config::SetBaseOrCurrent(Config::MAIN_ANALYTICS_ENABLED, response);
+  
+  [FIRAnalytics setAnalyticsCollectionEnabled:response];
+  
+  [self.navigationController popViewControllerAnimated:true];
+}
+
+- (IBAction)optInPressed:(id)sender {
+  [self HandleResponse:true];
+}
+
+- (IBAction)optOutPressed:(id)sender {
+  [self HandleResponse:false];
+}
+
+@end
