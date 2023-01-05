65ba326b2bb88d68b9e3777624bd0e4e4daebfa2
OatmealDome <julian@oatmealdome.me>
2023-01-04 19:17:15 -0500

    Package: Add files for debs

---
+++ Source/iOS/App/Project/Package/Deb/postinst.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+# Seriously, WTF is going on here? I'm really curious what app/tweak
+# does this and why.
+echo "Checking if Documents is a file"
+if [ -f "/private/var/mobile/Documents" ]; then
+  rm /private/var/mobile/Documents
+fi
+
+echo "Creating user directory if needed"
+mkdir -p /private/var/mobile/Documents/DolphiniOS || true
+
+echo "Setting user directory permissions"
+chown -R mobile:mobile /private/var/mobile/Documents/DolphiniOS
+
+echo "Running uicache"
+uicache
