3532fe84d668915f9718beec65e33d82f7762f4c
OatmealDome <julian@oatmealdome.me>
2023-01-20 19:22:06 -0500

    MappingDeviceViewController: Add prompt for importing defaults for a recognized device type

--- Source/iOS/App/Common/UI/Settings/Mapping/Device/MappingDeviceViewController.mm
+++ Source/iOS/App/Common/UI/Settings/Mapping/Device/MappingDeviceViewController.mm
@@ -6,8 +6,11 @@
 #import <string>
 #import <vector>
 
+#import "Common/FileUtil.h"
+
 #import "InputCommon/ControllerEmu/ControllerEmu.h"
 #import "InputCommon/ControllerInterface/ControllerInterface.h"
+#import "InputCommon/InputConfig.h"
 
 #import "FoundationStringUtil.h"
 #import "LocalizationUtil.h"
@@ -118,7 +121,12 @@ struct Device {
 
 - (void)tableView:(UITableView*)tableView didSelectRowAtIndexPath:(NSIndexPath*)indexPath {
   if (_lastSelected != indexPath.row) {
-    self.emulatedController->SetDefaultDevice(_devices[indexPath.row]);
+    const std::string device = _devices[indexPath.row];
+    
+    ciface::Core::DeviceQualifier qualifier;
+    qualifier.FromString(device);
+    
+    self.emulatedController->SetDefaultDevice(device);
     self.emulatedController->UpdateReferences(g_controller_interface);
     
     MappingDeviceCell* cell = [tableView cellForRowAtIndexPath:indexPath];
@@ -130,6 +138,36 @@ struct Device {
     }
     
     _lastSelected = indexPath.row;
+    
+    bool isTouchscreen = qualifier.source == "iOS";
+    bool isMFiPhysicalController = qualifier.source == "MFi" && qualifier.name != "Keyboard";
+    
+    if (isTouchscreen || isMFiPhysicalController) {
+      UIAlertController* alertController = [UIAlertController alertControllerWithTitle:@"Load Defaults" message:@"Would you like to load the default profile for this device type?\n\nWARNING: If you choose to proceed, your current configuration will be overwritten." preferredStyle:UIAlertControllerStyleAlert];
+      
+      [alertController addAction:[UIAlertAction actionWithTitle:@"Load" style:UIAlertActionStyleDestructive handler:^(UIAlertAction*) {
+        std::string iniName;
+        
+        if (isTouchscreen) {
+          iniName = "Touchscreen";
+        } else {
+          iniName = "Physical Controller";
+        }
+        
+        const std::string profilePath = File::GetSysDirectory() + "Profiles/" + self.inputConfig->GetProfileName() + "/" + iniName + ".ini";
+        
+        IniFile iniFile;
+        iniFile.Load(profilePath);
+        
+        self.emulatedController->LoadConfig(iniFile.GetOrCreateSection("Profile"));
+        self.emulatedController->SetDefaultDevice(device);
+        self.emulatedController->UpdateReferences(g_controller_interface);
+      }]];
+      
+      [alertController addAction:[UIAlertAction actionWithTitle:@"Don't Load" style:UIAlertActionStyleCancel handler:nil]];
+      
+      [self presentViewController:alertController animated:true completion:nil];
+    }
     
     [self.delegate deviceDidChange:self];
   }
