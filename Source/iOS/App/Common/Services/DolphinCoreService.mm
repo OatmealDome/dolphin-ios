bdc6c307f21162d55f1a0c9656a95fbe703cadfd
OatmealDome <julian@oatmealdome.me>
2023-06-24 18:02:27 -0400

    DolphinCoreService: Use new host thread

--- Source/iOS/App/Common/Services/DolphinCoreService.mm
+++ Source/iOS/App/Common/Services/DolphinCoreService.mm
@@ -19,78 +19,85 @@
 
 #import "DolphiniOS-Swift.h"
 #import "EmulationCoordinator.h"
-#import "FoundationStringUtil.h"
-#import "LocalizationUtil.h"
 #import "FastmemManager.h"
+#import "FoundationStringUtil.h"
+#import "HostQueue.h"
+#import "LocalizationUtil.h"
 #import "MsgAlertManager.h"
 
 @implementation DolphinCoreService
 
 - (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary<UIApplicationLaunchOptionsKey,id>*)launchOptions {
-  Core::DeclareAsHostThread();
-  
-  UICommon::SetUserDirectory(FoundationToCppString([UserFolderUtil getUserFolder]));
-  UICommon::CreateDirectories();
-  UICommon::Init();
-  
-  [[MsgAlertManager shared] registerHandler];
-  
-  Common::RegisterStringTranslator([](const char* text) {
-    return FoundationToCppString(DOLCoreLocalizedString(CToFoundationString(text)));
-  });
-  
-  Config::SetBase(Config::MAIN_USE_GAME_COVERS, true);
-  
-  Config::SetBase(Config::MAIN_FASTMEM, [FastmemManager shared].fastmemAvailable);
-  
-  WindowSystemInfo wsi;
-  wsi.type = WindowSystemType::iOS;
-  
-  g_controller_interface.Initialize(wsi);
-  
-  Pad::Initialize();
-  Wiimote::Initialize(Wiimote::InitializeMode::DO_NOT_WAIT_FOR_WIIMOTES);
-  
-  Wiimote::LoadConfig();
-  Wiimote::GetConfig()->SaveConfig();
+  DOLHostQueueRunSync(^{
+    UICommon::SetUserDirectory(FoundationToCppString([UserFolderUtil getUserFolder]));
+    UICommon::CreateDirectories();
+    UICommon::Init();
+    
+    [[MsgAlertManager shared] registerHandler];
+    
+    Common::RegisterStringTranslator([](const char* text) {
+      return FoundationToCppString(DOLCoreLocalizedString(CToFoundationString(text)));
+    });
+    
+    Config::SetBase(Config::MAIN_USE_GAME_COVERS, true);
+    
+    Config::SetBase(Config::MAIN_FASTMEM, [FastmemManager shared].fastmemAvailable);
+    
+    WindowSystemInfo wsi;
+    wsi.type = WindowSystemType::iOS;
+    
+    g_controller_interface.Initialize(wsi);
+    
+    Pad::Initialize();
+    Wiimote::Initialize(Wiimote::InitializeMode::DO_NOT_WAIT_FOR_WIIMOTES);
+    
+    Wiimote::LoadConfig();
+    Wiimote::GetConfig()->SaveConfig();
 
-  Pad::LoadConfig();
-  Pad::GetConfig()->SaveConfig();
+    Pad::LoadConfig();
+    Pad::GetConfig()->SaveConfig();
+  });
 
   return YES;
 }
 
 - (void)applicationDidBecomeActive:(UIApplication*)application {
-  if (Core::IsRunning() && ![EmulationCoordinator shared].userRequestedPause) {
-    Core::SetState(Core::State::Running);
-  }
+  DOLHostQueueRunSync(^{
+    if (Core::IsRunning() && ![EmulationCoordinator shared].userRequestedPause) {
+      Core::SetState(Core::State::Running);
+    }
+  });
 }
 
 - (void)applicationWillResignActive:(UIApplication*)application {
-  if (Core::IsRunning() && ![EmulationCoordinator shared].userRequestedPause) {
-    Core::SetState(Core::State::Paused);
-  }
-  
-  // Write out the configuration in case we don't get a chance later
-  Config::Save();
+  DOLHostQueueRunSync(^{
+    if (Core::IsRunning() && ![EmulationCoordinator shared].userRequestedPause) {
+      Core::SetState(Core::State::Paused);
+    }
+    
+    // Write out the configuration in case we don't get a chance later
+    Config::Save();
+  });
 }
 
 - (void)applicationWillTerminate:(UIApplication*)application {
-  if (Core::IsRunning()) {
-    Core::Stop();
+  DOLHostQueueRunSync(^{
+    if (Core::IsRunning()) {
+      Core::Stop();
+      
+      // Spin while Core stops
+      while (Core::GetState() != Core::State::Uninitialized) {}
+    }
     
-    // Spin while Core stops
-    while (Core::GetState() != Core::State::Uninitialized) {}
-  }
-  
-  Pad::Shutdown();
-  Wiimote::Shutdown();
-  g_controller_interface.Shutdown();
-  
-  Config::Save();
-  
-  Core::Shutdown();
-  UICommon::Shutdown();
+    Pad::Shutdown();
+    Wiimote::Shutdown();
+    g_controller_interface.Shutdown();
+    
+    Config::Save();
+    
+    Core::Shutdown();
+    UICommon::Shutdown();
+  });
 }
 
 @end
