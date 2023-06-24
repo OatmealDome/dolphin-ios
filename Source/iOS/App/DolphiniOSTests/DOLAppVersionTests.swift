98f5b12879a64bc8c0cb966c2fc02a2ab600d176
OatmealDome <julian@oatmealdome.me>
2023-06-24 04:25:53 -0400

    Tests: Add tests for DOLAppVersion's implemenation of Comparable protocol

--- Source/iOS/App/DolphiniOSTests/DOLAppVersionTests.swift
+++ Source/iOS/App/DolphiniOSTests/DOLAppVersionTests.swift
@@ -36,4 +36,59 @@ class DOLAppVersionTests: XCTestCase {
   func testEqualNonBeta() throws {
     XCTAssertEqual(DOLAppVersion(jsonVersion: "11.22.33 (55)"), DOLAppVersion(jsonVersion: "11.22.33 (55)"))
   }
+  
+  func testLessThanLargerMajor() throws {
+    XCTAssertLessThan(DOLAppVersion(jsonVersion: "11.22.33 (55)"), DOLAppVersion(jsonVersion: "99.22.33 (55)"))
+  }
+  
+  func testLessThanLargerMinor() throws {
+    XCTAssertLessThan(DOLAppVersion(jsonVersion: "11.22.33 (55)"), DOLAppVersion(jsonVersion: "11.99.33 (55)"))
+  }
+  
+  func testLessThanLargerPatch() throws {
+    XCTAssertLessThan(DOLAppVersion(jsonVersion: "11.22.33 (55)"), DOLAppVersion(jsonVersion: "11.22.99 (55)"))
+  }
+  
+  func testLessThanLargerBeta() throws {
+    XCTAssertLessThan(DOLAppVersion(jsonVersion: "11.22.33b44 (55)"), DOLAppVersion(jsonVersion: "11.22.33b99 (55)"))
+  }
+  
+  func testLessThanLargerBuild() throws {
+    XCTAssertLessThan(DOLAppVersion(jsonVersion: "11.22.33b44 (55)"), DOLAppVersion(jsonVersion: "11.22.33 (99)"))
+  }
+  
+  func testLessThanBetaAndNonBeta() throws {
+    XCTAssertLessThan(DOLAppVersion(jsonVersion: "11.22.33b44 (55)"), DOLAppVersion(jsonVersion: "11.22.33 (55)"))
+  }
+  
+  //
+  
+  func testGreaterThanLargerMajor() throws {
+    XCTAssertGreaterThan(DOLAppVersion(jsonVersion: "99.22.33b44 (55)"), DOLAppVersion(jsonVersion: "11.22.33b44 (55)"))
+  }
+  
+  func testGreaterThanLargerMinor() throws {
+    XCTAssertGreaterThan(DOLAppVersion(jsonVersion: "11.99.33b44 (55)"), DOLAppVersion(jsonVersion: "11.22.33b44 (55)"))
+  }
+  
+  func testGreaterThanLargerPatch() throws {
+    XCTAssertGreaterThan(DOLAppVersion(jsonVersion: "11.22.99b44 (55)"), DOLAppVersion(jsonVersion: "11.22.33b44 (55)"))
+  }
+  
+  func testGreaterThanLargerBeta() throws {
+    XCTAssertGreaterThan(DOLAppVersion(jsonVersion: "11.22.33b99 (55)"), DOLAppVersion(jsonVersion: "11.22.33b44 (55)"))
+  }
+  
+  func testGreaterThanLargerBuild() throws {
+    XCTAssertGreaterThan(DOLAppVersion(jsonVersion: "11.22.33b44 (99)"), DOLAppVersion(jsonVersion: "11.22.33b44 (55)"))
+  }
+  
+  func testGreaterThanBetaAndNonBeta() throws {
+    XCTAssertGreaterThan(DOLAppVersion(jsonVersion: "11.22.33 (55)"), DOLAppVersion(jsonVersion: "11.22.33b44 (55)"))
+  }
+  
+  func testNotLessThanSameVersion() throws {
+    XCTAssertGreaterThan(DOLAppVersion(jsonVersion: "11.22.33 (55)"), DOLAppVersion(jsonVersion: "11.22.33 (55)"))
+  }
 }
+
