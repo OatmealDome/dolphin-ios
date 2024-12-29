08ee29dcf8809be1097ec2b7185fbae0ff53454c
OatmealDome <julian@oatmealdome.me>
2024-12-29 15:08:08 -0500

    TCWiiTouchIRMode: Add mode enum

---
+++ Source/iOS/App/DolphiniOS/UI/Emulation/TouchController/TCWiiTouchIRMode.swift
@@ -0,0 +1,10 @@
+// Copyright 2024 DolphiniOS Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+import Foundation
+
+@objc enum TCWiiTouchIRMode: Int {
+  case none = 0
+  case follow = 1
+  case drag = 2
+}
