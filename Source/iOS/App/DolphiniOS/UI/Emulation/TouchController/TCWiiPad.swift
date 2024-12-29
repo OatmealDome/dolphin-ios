6f5ea80fff3afb08aa945f39c7038c71f9badefa
OatmealDome <julian@oatmealdome.me>
2024-12-29 02:28:47 -0500

    TCWiiPad: Re-implement based on Android's InputOverlayPointer

--- Source/iOS/App/DolphiniOS/UI/Emulation/TouchController/TCWiiPad.swift
+++ Source/iOS/App/DolphiniOS/UI/Emulation/TouchController/TCWiiPad.swift
@@ -5,16 +5,15 @@
 import Foundation
 import UIKit
 
-class TCWiiPad: TCView, UIGestureRecognizerDelegate
-{
-  var touch_ir_enabled: Bool = true
-  var current_rect: CGRect? = nil
-  var max_size: CGSize = CGSize.zero
-  var aspect_adjusted: CGFloat = 0
-  var x_adjusted: Bool = false
+class TCWiiPad: TCView, UIGestureRecognizerDelegate {
+  var touchPointerEnabled: Bool = true
   
-  required init?(coder: NSCoder)
-  {
+  var gameCenterX: CGFloat = 0
+  var gameCenterY: CGFloat = 0
+  var gameWidthHalfInv: CGFloat = 0
+  var gameHeightHalfInv: CGFloat = 0
+  
+  required init?(coder: NSCoder) {
     super.init(coder: coder)
     
     // Register our "long press" gesture recognizer
@@ -26,101 +25,63 @@ class TCWiiPad: TCView, UIGestureRecognizerDelegate
     self.real_view!.addGestureRecognizer(pressHandler)
   }
   
-  // Based on InputOverlayPointer.java
-  @objc func recalculatePointerValues(new_rect: CGRect, game_aspect: CGFloat)
-  {
-    // Set rect
-    self.current_rect = new_rect
-    
-    // Adjusting for black bars
-    let device_aspect: CGFloat = new_rect.width / new_rect.height
-    self.aspect_adjusted = game_aspect / device_aspect;
-    
-    if (game_aspect <= device_aspect) // black bars on left/right
-    {
-      self.x_adjusted = true
-      
-      let game_x = round(new_rect.height * game_aspect)
-      let buffer = new_rect.width - game_x
-      
-      self.max_size = CGSize(width: (new_rect.width - buffer) / 2, height: new_rect.height / 2)
+  @objc func recalculatePointerValues(new_rect: CGRect, game_aspect: CGFloat) {
+    gameCenterX = new_rect.midX
+    gameCenterY = new_rect.midY
+
+    var gameWidth = new_rect.width
+    var gameHeight = new_rect.height
+
+    // Adjusting for device's black bars.
+    let surfaceAR = gameWidth / gameHeight
+    let gameAR = game_aspect
+    if (gameAR <= surfaceAR) {
+        // Black bars on left/right
+        gameWidth = gameHeight * gameAR
+    } else {
+        // Black bars on top/bottom
+        gameHeight = gameWidth / gameAR
     }
-    else // bars on top and bottom
-    {
-      self.x_adjusted = false
-      
-      let game_y = round(new_rect.width / game_aspect)
-      let buffer = new_rect.height - game_y
-      
-      self.max_size = CGSize(width: (new_rect.width / 2), height: (new_rect.height - buffer) / 2)
-    }
+
+    gameWidthHalfInv = 1 / (gameWidth * 0.5)
+    gameHeightHalfInv = 1 / (gameHeight * 0.5)
   }
   
-  func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool
-  {
-    if (touch.view == self.real_view)
-    {
+  func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+    if (touch.view == self.real_view) {
       return true
     }
     
     return false
   }
   
-  func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer) -> Bool
-  {
-    if (otherGestureRecognizer is UIScreenEdgePanGestureRecognizer)
-    {
+  func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+    if (otherGestureRecognizer is UIScreenEdgePanGestureRecognizer) {
       return true
     }
     
     return false
   }
   
-  @objc func handleLongPress(gesture: UILongPressGestureRecognizer)
-  {
-    if (!self.touch_ir_enabled)
-    {
+  @objc func handleLongPress(gesture: UILongPressGestureRecognizer) {
+    if (!self.touchPointerEnabled) {
       return
     }
     
-    guard let image_rect = self.current_rect else { return }
-
-    // Convert the point to the native screen scale
-    let screen_scale = UIScreen.main.scale
-    var point = gesture.location(in: self)
-    point.x *= screen_scale
-    point.y *= screen_scale
+    let point = gesture.location(in: self)
+    
     var axises: (y: CGFloat, x: CGFloat) = (0, 0)
-
-    // Check if this touch is within the rendering rectangle
-    if point.y >= image_rect.origin.y && point.y <= image_rect.origin.y + image_rect.height
-    {
-      // Move the point to the origin of the rectangle
-      point.x = point.x - image_rect.origin.x
-      point.y = point.y - image_rect.origin.y
-
-      if self.x_adjusted
-      {
-        axises.y = (point.y - self.max_size.height) / self.max_size.height
-        axises.x = ((point.x * self.aspect_adjusted) - self.max_size.width) / self.max_size.width
-      }
-      else
-      {
-        axises.y = ((point.y * self.aspect_adjusted) - self.max_size.height) / self.max_size.height
-        axises.x = (point.x - self.max_size.width) / self.max_size.width
-      }
-
-      let axisStartIdx = TCButtonType.wiiInfrared
-      for (i, axis) in [axises.y, axises.y, axises.x, axises.x].enumerated()
-      {
-        TCManagerInterface.setAxisValueFor(axisStartIdx.rawValue + i + 1, controller: self.port, value: Float(axis))
-      }
+    
+    axises.x = (point.x - gameCenterX) * gameWidthHalfInv
+    axises.y = (point.y - gameCenterY) * gameHeightHalfInv
+    
+    let axisStartIdx = TCButtonType.wiiInfrared
+    for (i, axis) in [axises.y, axises.y, axises.x, axises.x].enumerated() {
+      TCManagerInterface.setAxisValueFor(axisStartIdx.rawValue + i + 1, controller: self.port, value: Float(axis))
     }
   }
   
-  @objc func setTouchPointerEnabled(_ enabled: Bool)
-  {
-    self.touch_ir_enabled = enabled
+  @objc func setTouchPointerEnabled(_ enabled: Bool) {
+    self.touchPointerEnabled = enabled
   }
-  
 }
