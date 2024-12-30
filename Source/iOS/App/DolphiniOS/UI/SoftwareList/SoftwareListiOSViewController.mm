88619dfe7bed696ad8e0b93659756e1879db493a
OatmealDome <julian@oatmealdome.me>
2024-12-30 02:34:02 -0500

    SoftwareListiOSViewController: Add import NAND to Wii menu

--- Source/iOS/App/DolphiniOS/UI/SoftwareList/SoftwareListiOSViewController.mm
+++ Source/iOS/App/DolphiniOS/UI/SoftwareList/SoftwareListiOSViewController.mm
@@ -9,6 +9,8 @@
 #import "Core/IOS/ES/ES.h"
 #import "Core/IOS/IOS.h"
 
+#import "DiscIO/NANDImporter.h"
+
 #import "UICommon/GameFile.h"
 
 #import "EmulationBootParameter.h"
@@ -18,8 +20,9 @@
 #import "LocalizationUtil.h"
 
 typedef NS_ENUM(NSInteger, DOLSoftwareListDocumentPickerType) {
-  DOLSoftwareListDocumentPickerTypeImport,
-  DOLSoftwareListDocumentPickerTypeOpenExternal
+  DOLSoftwareListDocumentPickerTypeImportSoftware,
+  DOLSoftwareListDocumentPickerTypeImportNAND,
+  DOLSoftwareListDocumentPickerTypeOpenExternal,
 };
 
 @implementation SoftwareListiOSViewController {
@@ -49,6 +52,14 @@ typedef NS_ENUM(NSInteger, DOLSoftwareListDocumentPickerType) {
   
   NSArray<UIMenuElement*>* wiiActions;
   
+  UIMenuElement* wiiNandElement = [UIMenu menuWithTitle:DOLCoreLocalizedString(@"Manage NAND") children:@[
+    [UIAction actionWithTitle:DOLCoreLocalizedString(@"Import BootMii NAND Backup...") image:[UIImage systemImageNamed:@"internaldrive"] identifier:nil handler:^(UIAction*) {
+      [self openDocumentPickerWithContentTypes:@[
+        [UTType typeWithFilenameExtension:@"bin"]
+      ] pickerType:DOLSoftwareListDocumentPickerTypeImportNAND];
+    }]
+  ]];
+  
   // Get the system menu TMD
   IOS::HLE::Kernel ios;
   const auto tmd = ios.GetESCore().FindInstalledTMD(Titles::SYSTEM_MENU);
@@ -71,12 +82,14 @@ typedef NS_ENUM(NSInteger, DOLSoftwareListDocumentPickerType) {
         
         [self performSegueWithIdentifier:@"emulation" sender:nil];
       }],
+      wiiNandElement,
       [UIAction actionWithTitle:DOLCoreLocalizedString(@"Perform Online System Update") image:[UIImage systemImageNamed:@"icloud.and.arrow.down"] identifier:nil handler:^(UIAction*) {
         [self performSegueForWiiUpdateWithSource:@"" isOnline:true];
       }]
     ];
   } else {
     wiiActions = @[
+      wiiNandElement,
       [UIMenu menuWithTitle:DOLCoreLocalizedString(@"Perform Online System Update") image:[UIImage systemImageNamed:@"icloud.and.arrow.down"] identifier:nil options:0 children:@[
         [UIAction actionWithTitle:DOLCoreLocalizedString(@"Europe") image:nil identifier:nil handler:^(UIAction*) {
           [self performSegueForWiiUpdateWithSource:@"EUR" isOnline:true];
@@ -125,22 +138,22 @@ typedef NS_ENUM(NSInteger, DOLSoftwareListDocumentPickerType) {
 }
 
 - (IBAction)addButtonPressed:(id)sender {
-  [self openDocumentPickerWithSoftwareContentTypesAndPickerType:DOLSoftwareListDocumentPickerTypeImport];
+  [self openDocumentPickerWithSoftwareContentTypesAndPickerType:DOLSoftwareListDocumentPickerTypeImportSoftware];
 }
 
 - (void)documentPicker:(UIDocumentPickerViewController*)controller didPickDocumentsAtURLs:(NSArray<NSURL*>*)urls {
-  if (_pickerType == DOLSoftwareListDocumentPickerTypeImport) {
+  void (^showError)(NSString*) = ^(NSString* error) {
+    UIAlertController* errorAlert = [UIAlertController alertControllerWithTitle:DOLCoreLocalizedString(@"Error") message:error preferredStyle:UIAlertControllerStyleAlert];
+    
+    [errorAlert addAction:[UIAlertAction actionWithTitle:DOLCoreLocalizedString(@"OK") style:UIAlertActionStyleDefault
+      handler:nil]];
+    
+    [self presentViewController:errorAlert animated:true completion:nil];
+  };
+  
+  if (_pickerType == DOLSoftwareListDocumentPickerTypeImportSoftware) {
     [[ImportFileManager shared] importFileAtUrl:urls[0]];
   } else if (_pickerType == DOLSoftwareListDocumentPickerTypeOpenExternal) {
-    void (^showError)(NSString*) = ^(NSString* error) {
-      UIAlertController* errorAlert = [UIAlertController alertControllerWithTitle:DOLCoreLocalizedString(@"Error") message:error preferredStyle:UIAlertControllerStyleAlert];
-      
-      [errorAlert addAction:[UIAlertAction actionWithTitle:DOLCoreLocalizedString(@"OK") style:UIAlertActionStyleDefault
-        handler:nil]];
-      
-       [self presentViewController:errorAlert animated:true completion:nil];
-    };
-    
     NSURL* url = urls[0];
     
     if (![url startAccessingSecurityScopedResource]) {
@@ -164,6 +177,33 @@ typedef NS_ENUM(NSInteger, DOLSoftwareListDocumentPickerType) {
     }
     
     [self loadGameFile:gameFileWrapper];
+  } else if (_pickerType == DOLSoftwareListDocumentPickerTypeImportNAND) {
+    NSURL* url = urls[0];
+    
+    if (![url startAccessingSecurityScopedResource]) {
+      showError(@"Failed to start accessing security scoped resource.");
+      return;
+    }
+    
+    UIAlertController* waitAlert = [UIAlertController alertControllerWithTitle:DOLCoreLocalizedString(@"Importing NAND backup") message:nil preferredStyle:UIAlertControllerStyleAlert];
+    
+    [self presentViewController:waitAlert animated:true completion:^{
+      dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), ^{
+        DiscIO::NANDImporter().ImportNANDBin(FoundationToCppString([url path]), [] {
+          // Called to update the GUI. We don't need to do this.
+        }, [] {
+          // Called if we need to find NAND keys. Android doesn't implement this, so let's not do it either.
+          PanicAlertFmtT("The decryption keys need to be appended to the NAND backup file.");
+          return "";
+        });
+        
+        [url stopAccessingSecurityScopedResource];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+          [waitAlert dismissViewControllerAnimated:true completion:nil];
+        });
+      });
+    }];
   }
 }
 
